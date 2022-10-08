13ab22af2133da5b63b64615241784de6bf29ef7
Min <solunar@vip.qq.com>
2022-10-08 14:56:17 +0800

    Initial

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, nesciunt?</div>
+</template>
+
+<script>
+import * as THREE from 'three';
+
+export default {
+  name: 'home-view',
+  mounted() {
+    console.log(THREE.REVISION);
+  }
+};
+</script>
