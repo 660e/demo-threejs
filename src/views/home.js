911db52f6eeb1b1b1bf120d91df2a805847ce084
Min <solunar@vip.qq.com>
2022-10-09 11:09:24 +0800

    Update

---
+++ src/views/home.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+camera.position.z = 3;
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const cube = new THREE.Mesh(geometry, material);
+
+scene.add(cube);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+export function init(el) {
+  document.querySelector(el).appendChild(renderer.domElement);
+  animate();
+}
--- src/views/home.vue
+++ src/views/home.vue
@@ -3,38 +3,12 @@
 </template>
 
 <script>
-import * as THREE from 'three';
+import { init } from './home.js';
 
 export default {
   name: 'home-view',
-  data() {
-    return {
-      camera: new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000),
-      renderer: new THREE.WebGLRenderer()
-    };
-  },
   mounted() {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    document.querySelector('#canvas').appendChild(this.renderer.domElement);
-
-    const scene = new THREE.Scene();
-
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    const cube = new THREE.Mesh(geometry, material);
-
-    scene.add(cube);
-    this.camera.position.z = 3;
-
-    this.animate(scene, cube);
-  },
-  methods: {
-    animate(scene, cube) {
-      requestAnimationFrame(() => this.animate(scene, cube));
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      this.renderer.render(scene, this.camera);
-    }
+    init('#canvas');
   }
 };
 </script>
